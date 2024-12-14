10ab35d46c4f25902f8f70bfa90844869920c98f
CoralCarl <tim.heumer@gmail.com>
2024-12-14 18:48:41 +0100

    add 2024-09

---
+++ crates/aoc2024/src/day_09.rs
@@ -0,0 +1,114 @@
+#![allow(unused_variables)]
+
+pub fn part1(input: &str) -> String {
+    let files = input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .enumerate()
+        .map(|(i, v)| (i / 2, v))
+        .collect::<Vec<(usize, u64)>>();
+    let mut files_it = files.iter();
+
+    let mut checksum = 0;
+    let mut id = 0;
+
+    let (mut blk_back, mut size_back) = files_it.next_back().unwrap();
+
+    let mut even = true;
+
+    'outer: while let Some((blk, size)) = files_it.next() {
+        if even {
+            for _ in id..id + size {
+                checksum += id * *blk as u64;
+                id += 1;
+            }
+        } else {
+            for _ in id..id + size {
+                if size_back == 0 {
+                    if files_it.next_back().is_none() {
+                        break 'outer;
+                    }
+                    (blk_back, size_back) = match files_it.next_back() {
+                        Some(value) => *value,
+                        None => {
+                            break 'outer;
+                        }
+                    };
+                }
+
+                checksum += id * blk_back as u64;
+                size_back -= 1;
+                id += 1;
+            }
+        }
+        even ^= true;
+    }
+
+    for id in id..id + size_back {
+        checksum += id * blk_back as u64;
+    }
+
+    checksum.to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    let mut files = Vec::new();
+    let mut spaces = Vec::new();
+
+    let mut blk_offset = 0;
+
+    let mut even = true;
+    for (blk_id, c) in input.trim().chars().enumerate() {
+        let blk_size = c.to_digit(10).unwrap() as u64;
+        if even {
+            files.push((blk_offset, blk_size, blk_id / 2));
+        } else {
+            spaces.push((blk_offset, blk_size));
+        }
+        blk_offset += blk_size;
+        even ^= true;
+    }
+
+    let mut files_it = files.iter().rev();
+
+    let mut checksum = 0;
+
+    while let Some(&(blk_offset, blk_size, blk_id)) = files_it.next() {
+        let mut moved = false;
+        for (space_offset, space_size) in spaces.iter_mut() {
+            if *space_offset > blk_offset {
+                break;
+            }
+            if *space_size >= blk_size {
+                for file_id in *space_offset..*space_offset + blk_size {
+                    checksum += file_id * blk_id as u64;
+                }
+                *space_size -= blk_size;
+                *space_offset += blk_size;
+
+                moved = true;
+                break;
+            }
+        }
+        if !moved {
+            for file_id in blk_offset..blk_offset + blk_size {
+                checksum += file_id * blk_id as u64;
+            }
+        }
+    }
+
+    checksum.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = "2333133121414131402";
+        assert_eq!(part1(&input), "1928");
+        assert_eq!(part2(&input), "2858");
+    }
+}
--- crates/aoc2024/src/main.rs
+++ crates/aoc2024/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, time::Instant};
 
 mod day_01;
 mod day_02;
@@ -8,6 +8,7 @@ mod day_05;
 mod day_06;
 mod day_07;
 mod day_08;
+mod day_09;
 
 fn main() {
     let days: Vec<(fn(&str) -> String, fn(&str) -> String)> = vec![
@@ -19,19 +20,24 @@ fn main() {
         (day_06::part1, day_06::part2),
         (day_07::part1, day_07::part2),
         (day_08::part1, day_08::part2),
+        (day_09::part1, day_09::part2),
     ];
 
     for (day, (part1, part2)) in days.iter().enumerate() {
         let path = format!("input/{:02}.txt", day + 1);
         let file = read_to_string(&path);
 
-        println!("====== Day {:02} ======", day + 1);
+        println!("========= Day {:02} =========", day + 1);
         match file {
             Ok(input) => {
+                let mut start = Instant::now();
                 let solution1 = part1(&input);
+                let time1 = start.elapsed();
+                start = Instant::now();
                 let solution2 = part2(&input);
-                println!("Part 1: {solution1}");
-                println!("Part 2: {solution2}");
+                let time2 = start.elapsed();
+                println!("{:>7.3}s Part 1: {solution1}", time1.as_secs_f64());
+                println!("{:>7.3}s Part 2: {solution2}", time2.as_secs_f64());
             }
             Err(_) => {
                 println!("File not found");
