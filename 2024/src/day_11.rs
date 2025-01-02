70e645d57ec9014a2700b5a7d398b64f4b6ae224
CoralCarl <tim.heumer@gmail.com>
2025-01-02 23:54:07 +0100

    clean up 2024-11

--- 2024/src/day_11.rs
+++ 2024/src/day_11.rs
@@ -1,53 +1,53 @@
 #![allow(unused_variables)]
 
-fn blink(mut stones: Vec<u64>, count: usize) -> Vec<u64> {
-    for _ in 0..count {
-        let mut new_stones = Vec::new();
-        for stone in &stones {
-            if *stone == 0 {
-                new_stones.push(1);
+fn digit_count(mut num: u64) -> u64 {
+    let mut digit_count = 0;
+    while num > 0 {
+        num /= 10;
+        digit_count += 1;
+    }
+    digit_count
+}
+
+fn blink(stones: &mut Vec<u64>) {
+    for i in 0..stones.len() {
+        if stones[i] == 0 {
+            stones[i] = 1;
+        } else {
+            let digit_count = digit_count(stones[i]);
+            if digit_count % 2 == 0 {
+                let p = 10_u64.pow(digit_count as u32 / 2);
+                stones.push(stones[i] % p);
+                stones[i] /= p;
             } else {
-                let stone_length = stone.to_string().len();
-                if stone_length & 1 == 0 {
-                    let num = 10_u64.pow(stone_length as u32 / 2);
-                    new_stones.push(stone / num);
-                    new_stones.push(stone % num);
-                } else {
-                    new_stones.push(stone * 2024);
-                }
+                stones[i] *= 2024;
             }
         }
-        stones = new_stones;
     }
-    stones
+}
+
+fn parse(input: &str) -> Vec<u64> {
+    input
+        .split_whitespace()
+        .map(|num| num.parse::<u64>().unwrap())
+        .collect()
 }
 
 pub fn part1(input: &str) -> String {
-    blink(
-        input
-            .trim()
-            .split(' ')
-            .map(|num| num.parse::<u64>().unwrap())
-            .collect(),
-        25,
-    )
-    .len()
-    .to_string()
+    let mut stones = parse(input);
+    for _ in 0..25 {
+        blink(&mut stones);
+    }
+    stones.len().to_string()
 }
 
 #[allow(unreachable_code)]
 pub fn part2(input: &str) -> String {
-    return "disabled".to_string();
-    blink(
-        input
-            .trim()
-            .split(' ')
-            .map(|num| num.parse::<u64>().unwrap())
-            .collect(),
-        75,
-    )
-    .len()
-    .to_string()
+    let mut stones = parse(input);
+    for _ in 0..75 {
+        blink(&mut stones);
+    }
+    stones.len().to_string()
 }
 
 #[cfg(test)]
