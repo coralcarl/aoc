de01e6b39aa3b126ad9fc88625c73ce13b45ed4a
CoralCarl <tim.heumer@gmail.com>
2025-01-04 21:55:36 +0100

    changed 2024-16 to astar

--- 2024/src/day_16.rs
+++ 2024/src/day_16.rs
@@ -1,108 +1,181 @@
 #![allow(unused_variables)]
 
-struct Maze {
-    walls: Vec<Vec<bool>>,
-    start: (usize, usize),
-    end: (usize, usize),
-    height: usize,
-    width: usize,
+use std::collections::{BinaryHeap, HashMap};
+
+type Walls = Vec<Vec<bool>>;
+
+fn parse(input: &str) -> Walls {
+    input
+        .lines()
+        .map(|row| row.chars().map(|c| c == '#').collect())
+        .collect()
 }
 
-fn parse(input: &str) -> Maze {
-    let mut start = (0, 0);
-    let mut end = (0, 0);
-    let width = input.find('\n').unwrap() - 2;
-    let height = input.len() / (width + 2) - 2;
-    let mut walls = vec![vec![false; height]; width];
-
-    let mut x = 0;
-    let mut y = 0;
-    for c in input.chars() {
-        match c {
-            '\n' => {
-                x = 0;
-                y += 1;
-            }
-            '#' if x > 0 && y > 0 && x <= width && y <= height => {
-                walls[x - 1][y - 1] = true;
-            }
-            'S' => start = (x - 1, y - 1),
-            'E' => end = (x - 1, y - 1),
-            '#' | '.' => (),
-            _ => panic!("unknown character '{c}'"),
-        }
-        if c != '\n' {
-            x += 1;
-        }
-    }
-
-    Maze {
-        walls,
-        start,
-        end,
-        height,
-        width,
-    }
+#[derive(Debug, PartialOrd, Ord, Eq, PartialEq, Hash, Clone, Copy)]
+struct State {
+    pos: (usize, usize),
+    dir: (isize, isize),
 }
 
-fn find_min_path(
-    maze: &Maze,
-    position: (usize, usize),
-    score: u64,
-    direction: usize,
-    done: &mut Vec<Vec<Vec<u64>>>,
-) -> u64 {
-    if done.is_empty() {
-        done.extend(vec![
-            vec![vec![u64::MAX - 4000; maze.height]; maze.width];
-            4
-        ]);
-    }
-
-    if position == maze.end {
-        return score;
-    }
-
-    // check if already visited with better score
-    if score >= done[direction][position.0][position.1] {
-        return u64::MAX;
-    }
-    // check if already visted rotation with better score
-    for d in 1..4 {
-        if score > done[(direction + d) % 4][position.0][position.1] + d as u64 % 2 * 1000 {
-            return u64::MAX;
-        }
-    }
-
-    done[direction][position.0][position.1] = score;
-
-    let next_position = [
-        (position.0 + 1, position.1),
-        (position.0, position.1 + 1),
-        (position.0.wrapping_sub(1), position.1),
-        (position.0, position.1.wrapping_sub(1)),
-    ];
-
+fn neighbors(state: &State, walls: &Walls) -> Vec<(State, usize)> {
     [
-        (next_position[direction], direction, score + 1),
-        (position, (direction + 1) % 4, score + 1000),
-        (position, (direction + 3) % 4, score + 1000),
+        (
+            State {
+                pos: (
+                    state.pos.0.wrapping_add_signed(state.dir.0),
+                    state.pos.1.wrapping_add_signed(state.dir.1),
+                ),
+                dir: state.dir,
+            },
+            1,
+        ),
+        (
+            State {
+                pos: (
+                    state.pos.0.wrapping_add_signed(-state.dir.1),
+                    state.pos.1.wrapping_add_signed(state.dir.0),
+                ),
+                dir: (-state.dir.1, state.dir.0),
+            },
+            1001,
+        ),
+        (
+            State {
+                pos: (
+                    state.pos.0.wrapping_add_signed(state.dir.1),
+                    state.pos.1.wrapping_add_signed(-state.dir.0),
+                ),
+                dir: (state.dir.1, -state.dir.0),
+            },
+            1001,
+        ),
     ]
     .into_iter()
-    .map(|(p, d, s)| {
-        if p.0 >= maze.width || p.1 >= maze.height || maze.walls[position.0][position.1] {
-            return u64::MAX;
+    .filter_map(|(state, cost)| {
+        if !walls[state.pos.1][state.pos.0] {
+            Some((state, cost))
+        } else {
+            None
+        }
+    })
+    .collect()
+}
+
+fn heuristic(state: &State, end: (usize, usize)) -> usize {
+    if state.pos.0 != end.0 && state.pos.1 != end.1 {
+        return state.pos.0.abs_diff(end.0) + state.pos.1.abs_diff(end.1) + 1000;
+    } else {
+        return state.pos.0.abs_diff(end.0) + state.pos.1.abs_diff(end.1);
+    }
+}
+
+struct PState {
+    estimated_cost: usize,
+    current_cost: usize,
+    state: State,
+}
+
+impl PState {
+    fn new(estimated_cost: usize, current_cost: usize, state: State) -> Self {
+        Self {
+            estimated_cost,
+            current_cost,
+            state,
+        }
+    }
+}
+
+impl Eq for PState {}
+impl PartialEq for PState {
+    fn eq(&self, other: &Self) -> bool {
+        self.estimated_cost.eq(&other.estimated_cost) && self.current_cost.eq(&other.current_cost)
+    }
+}
+
+impl PartialOrd for PState {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for PState {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match other.estimated_cost.cmp(&self.estimated_cost) {
+            std::cmp::Ordering::Equal => self.current_cost.cmp(&other.current_cost),
+            ord => ord,
+        }
+    }
+}
+
+fn reverse_path(
+    visited: &HashMap<State, (Option<State>, usize)>,
+    current: &State,
+) -> Vec<(usize, usize)> {
+    let mut path = vec![current.pos];
+    let mut current_state = current;
+    while let Some((Some(parent), _)) = visited.get(&current_state) {
+        path.push(parent.pos);
+        current_state = parent;
+    }
+    path.reverse();
+    path
+}
+
+fn cheapest_path(maze: Walls) -> Option<(Vec<(usize, usize)>, usize)> {
+    let start = State {
+        pos: (1, maze.len() - 2),
+        dir: (1, 0),
+    };
+    let end = (maze[0].len() - 2, 1);
+
+    let mut open: BinaryHeap<PState> = BinaryHeap::new();
+    let mut visited: HashMap<State, (Option<State>, usize)> = HashMap::new();
+
+    open.push(PState::new(0, 0, start));
+    visited.insert(start, (None, 0));
+
+    while let Some(PState {
+        estimated_cost,
+        current_cost,
+        state: current,
+    }) = open.pop()
+    {
+        if current.pos == end {
+            return Some((reverse_path(&visited, &current), current_cost));
         }
 
-        find_min_path(maze, p, s, d, done)
-    })
-    .min()
-    .unwrap()
+        if current_cost > visited.get(&current).unwrap().1 {
+            continue;
+        }
+
+        for (successor, move_cost) in neighbors(&current, &maze) {
+            let new_cost = current_cost + move_cost;
+
+            match visited.entry(successor) {
+                std::collections::hash_map::Entry::Vacant(e) => {
+                    e.insert((Some(current), new_cost));
+                }
+                std::collections::hash_map::Entry::Occupied(mut e) => {
+                    if e.get().1 > new_cost {
+                        e.insert((Some(current), new_cost));
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            let h_cost = heuristic(&successor, end);
+
+            open.push(PState::new(new_cost + h_cost, new_cost, successor));
+        }
+    }
+
+    None
 }
 
 pub fn part1(input: &str) -> String {
-    let maze = parse(&input);
-    find_min_path(&maze, maze.start, 0, 0, &mut Vec::new()).to_string()
+    let (path, cost) = cheapest_path(parse(&input)).unwrap();
+    cost.to_string()
 }
 
 pub fn part2(input: &str) -> String {
