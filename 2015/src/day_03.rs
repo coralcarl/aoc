a5b18e845db3f14cda8299338b9480770daa270b
CoralCarl <tim.heumer@gmail.com>
2025-01-07 23:26:03 +0100

    add 2015-03

--- 2015/src/day_03.rs
+++ 2015/src/day_03.rs
@@ -1,9 +1,30 @@
-#[allow(unused_variables)]
-pub fn part1(input: &str) -> String {
-    "todo".to_string()
+use std::collections::HashSet;
+
+fn deliver_presents(input: &str) -> HashSet<isize> {
+    let mut pos = 0;
+    let mut visited = HashSet::from_iter([pos]);
+
+    for b in input.bytes() {
+        match b {
+            b'>' => pos += 1,
+            b'<' => pos -= 1,
+            b'v' => pos += 256,
+            b'^' => pos -= 256,
+            _ => (),
+        }
+        visited.insert(pos);
+    }
+    visited
 }
 
-#[allow(unused_variables)]
+pub fn part1(input: &str) -> String {
+    deliver_presents(&input).len().to_string()
+}
+
 pub fn part2(input: &str) -> String {
-    "todo".to_string()
+    let santa_input: String = input.chars().step_by(2).collect();
+    let robot_input: String = input.chars().skip(1).step_by(2).collect();
+    let mut visited = deliver_presents(&santa_input);
+    visited.extend(deliver_presents(&robot_input));
+    visited.len().to_string()
 }
--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -6,7 +6,7 @@ use std::{
 
 mod day_01;
 mod day_02;
-// mod day_03;
+mod day_03;
 // mod day_04;
 // mod day_05;
 // mod day_06;
@@ -37,7 +37,7 @@ fn main() {
     let solutions: Vec<Solution> = vec![
         (day_01::part1, day_01::part2),
         (day_02::part1, day_02::part2),
-        // (day_03::part1, day_03::part2),
+        (day_03::part1, day_03::part2),
         // (day_04::part1, day_04::part2),
         // (day_05::part1, day_05::part2),
         // (day_06::part1, day_06::part2),
